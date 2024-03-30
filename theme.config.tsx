77e5c21f4a21ba1492df1bb9a278fbabc7362054
Paul Tuck <paul.tuck@oxon.org>
2024-03-30 20:44:17 +0700

    first commit

---
+++ theme.config.tsx
@@ -0,0 +1,17 @@
+import type { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  logo: <span>PAUL TUCK</span>,
+  project: {
+    link: 'https://github.com/shuding/nextra-docs-template',
+  },
+  chat: {
+    link: 'https://discord.com',
+  },
+  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  footer: {
+    text: 'Nextra Docs Template',
+  },
+}
+
+export default config
